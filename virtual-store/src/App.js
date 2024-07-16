e6955934d15eff84fe8254af1e06e5dd67229716
João Pedro Vogel Michalski <joaomichalskiti@gmail.com>
2024-07-16 17:40:35 -0300

    Primeiro Comit
    Início do projeto. Instalação dos pacotes utilizados.

---
+++ virtual-store/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header/>
+        <img src='./solveligth.png'/>
+        
+      <header/>
+    </div>
+  );
+}
+
+export default App;
