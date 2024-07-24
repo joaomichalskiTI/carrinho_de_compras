f9ff9950cd462dd148702673843759f054883304
João Pedro Vogel Michalski <joaomichalskiti@gmail.com>
2024-07-24 16:55:36 -0300

    Ajuste css

--- src/components/notes/index.jsx
+++ src/components/notes/index.jsx
@@ -1,15 +1,17 @@
 import "./styles.css";
 
 function notes(props) {
-    <div className="money_note_content">
-        <div>
-            <img src={props.src} alt="Hundred" />
+    return (
+        <div className="money_note_content">
+            <div>
+                <img src={props.src} alt="Nota" />
+            </div>
+            <div className="text_money_note">
+                {props.label1 && <p>{props.label1}</p>}
+                {props.label2 && <h2>{props.label2}</h2>}
+            </div>
         </div>
-        <div className="text_money_note">
-            {props.label1 && <p>{props.label1}</p>}
-            {props.label2 && <h2>{props.label2}</h2>}
-        </div>
-    </div>;
+    );
 }
 
 export default notes;
